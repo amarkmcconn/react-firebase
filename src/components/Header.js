d917cf179d5f5ca7b34b27cace8d38b20a873270
Mark McConnell <amark.mcconnell@gmail.com>
2022-07-11 13:03:01 -0700

    update project with crud

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,14 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 function Header(){
   return (
-    <h1>Help Queue</h1>
+    <motion.div
+      whileInView={{ x: [-100, 0], opacity: [0,1]}}
+      transition={{duration: 1}}
+    >
+      <h1>Help Queue</h1>
+    </motion.div>
   );
 }
 
--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -7,6 +7,7 @@ import { useFirestore } from 'react-redux-firebase'
 
 function NewTicketForm(props){
   const firestore = useFirestore();
+  
   function addTicketToFireStone(event) {
     event.preventDefault();
     props.onNewTicketCreation();
